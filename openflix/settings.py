cf59cc4c730ca9ccfd7d10563632ca562e386aee
siteflask1 <leandrolimahugo@gmail.com>
2024-01-21 02:30:29 -0300

    first commit

--- openflix/settings.py
+++ openflix/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -38,6 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "filme",
+    'bootstrap4',
+    'django_bootstrap_icons',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
