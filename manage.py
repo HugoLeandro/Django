9a65a842adb257e180e6e9e42f3af71266f20a59
siteflask1 <leandrolimahugo@gmail.com>
2024-01-21 06:35:34 -0300

    update

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.contrib.auth import get_user_model
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openflix.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openflix.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,8 +17,15 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
     # Cria o superusuário se ele ainda não existir
+    User = get_user_model()
+    email = os.getenv("EMAIL_ADMIN")
+    senha = os.getenv("SENHA_ADMIN")
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin', email=email, password=senha, is_active=True, is_staff=True)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
