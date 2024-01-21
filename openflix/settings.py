3c40606ff5328e27ccc2e0e48f4c6bed977dbd8a
siteflask1 <leandrolimahugo@gmail.com>
2024-01-21 07:31:30 -0300

    update

--- openflix/settings.py
+++ openflix/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django_bootstrap_icons',
     "filme",
     'bootstrap4',
     'crispy_forms',
